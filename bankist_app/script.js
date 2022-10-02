b30953fa011215e4a7b56197e539a05af9f41cee
PascalLiPooKim <paswei98@gmail.com>
2022-10-02 14:47:23 +0100

    Implement Login

--- bankist_app/script.js
+++ bankist_app/script.js
@@ -79,27 +79,27 @@ const displayMovements = function (movements) {
     containerMovements.insertAdjacentHTML("afterbegin", html);
   })
 }
-displayMovements(account1.movements);
+// displayMovements(account1.movements);
 
 const calcDisplayBalance = function (movements) {
   const balance = movements.reduce((acc, mov) => acc + mov, 0);
   labelBalance.textContent = `${balance} £`;
 };
 
-calcDisplayBalance(account1.movements);
+// calcDisplayBalance(account1.movements);
 
-const calcDisplaySummary = function (movements) {
-  const incomes = movements.filter(mov => mov > 0)
+const calcDisplaySummary = function (acc) {
+  const incomes = acc.movements.filter(mov => mov > 0)
     .reduce((acc, mov) => acc + mov, 0);
   
   labelSumIn.textContent = `${incomes} £`;
 
 
-  const out = movements.filter(mov => mov < 0).reduce((acc, mov) => acc + mov, 0);
+  const out = acc.movements.filter(mov => mov < 0).reduce((acc, mov) => acc + mov, 0);
   labelSumOut.textContent = `${Math.abs(out)} £`;
 
-  const interest = movements.filter(mov => mov > 0)
-    .map(deposit => deposit * 1.2 / 100)
+  const interest = acc.movements.filter(mov => mov > 0)
+    .map(deposit => (deposit * acc.interestRate) / 100)
     .filter((int, i, arr) => {
       return int >= 1;
     })
@@ -108,7 +108,7 @@ const calcDisplaySummary = function (movements) {
   labelSumInterest.textContent = `${interest} £`;
 };
 
-calcDisplaySummary(account1.movements)
+// calcDisplaySummary(account1.movements)
 
 const createUsernames = function (accs) {
 
@@ -121,6 +121,42 @@ const createUsernames = function (accs) {
 createUsernames(accounts);
 // console.log(accounts);
 
+// Event handler
+let currentAccount;
+
+btnLogin.addEventListener('click', function (e) {
+  // Prevent form from submitting
+  e.preventDefault();
+  currentAccount = accounts.find(acc => acc.username ===
+    inputLoginUsername.value);
+  
+  console.log(currentAccount);
+
+  if (currentAccount?.pin === Number(inputLoginPin.value)) {
+    // Display UI and message
+    labelWelcome.textContent = `Welcome back, 
+    ${currentAccount.owner.split(' ')[0]}`;
+
+    containerApp.style.opacity = 100;
+
+    // Clear input fields
+    inputLoginUsername.value = inputLoginPin.value = '';
+    inputLoginPin.blur();
+
+    // Display movements
+    displayMovements(currentAccount.movements);
+
+    // Display balance
+    calcDisplayBalance(currentAccount.movements);
+    // Display summary
+    calcDisplaySummary(currentAccount);
+  }
+  else {
+    containerApp.style.opacity = 0;
+    inputLoginUsername.value = inputLoginPin.value = '';
+    labelWelcome.textContent = `Invalid Credentials`;
+  }
+});
 
 
 
