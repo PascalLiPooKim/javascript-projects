3a115d30c23543820dcadf8a32cf8dd3fc9416c1
PascalLiPooKim <paswei98@gmail.com>
2022-10-08 16:25:28 +0100

    Implement smooth scrolling on "Learn more"

--- bankist_website/script.js
+++ bankist_website/script.js
@@ -55,4 +55,38 @@ message.style.backgroundColor = '#37383d';
 message.style.width = '120%';
   
 message.style.height =
-Number.parseFloat(getComputedStyle(message).height, 10) + 30 + 'px';
+  Number.parseFloat(getComputedStyle(message).height, 10) + 30 + 'px';
+
+
+const btnScrollTo = document.querySelector('.btn--scroll-to');
+const section1 = document.querySelector('#section--1');
+
+// Button scrolling
+btnScrollTo.addEventListener('click', function (e) {
+  const s1coords = section1.getBoundingClientRect();
+  console.log(s1coords);
+
+  console.log(e.target.getBoundingClientRect());
+
+  console.log('Current scroll (X/Y)', window.pageXOffset, window.pageYOffset);
+
+  console.log(
+    'height/width viewport',
+    document.documentElement.clientHeight,
+    document.documentElement.clientWidth
+  );
+
+  // Scrolling
+  // window.scrollTo(
+  //   s1coords.left + window.pageXOffset,
+  //   s1coords.top + window.pageYOffset
+  // );
+
+  // window.scrollTo({
+  //   left: s1coords.left + window.pageXOffset,
+  //   top: s1coords.top + window.pageYOffset,
+  //   behavior: 'smooth',
+  // });
+
+  section1.scrollIntoView({ behavior: 'smooth' });
+});
